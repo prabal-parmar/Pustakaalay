6b1feeca61b2b0bb575335c1be9e54e45dae5ba6
Prabal Parmar <prabal9713@gmail.com>
2025-12-20 17:57:39 +0530

    Setup frontend for auth and seller and buyer

--- PustakFrontPage/app/(buyer)/_layout.tsx
+++ PustakFrontPage/app/(buyer)/_layout.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
@@ -28,6 +28,13 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
         }}
       />
     </Tabs>
--- PustakFrontPage/app/_layout.tsx
+++ PustakFrontPage/app/_layout.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useState } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,12 +12,23 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [role, setRole] = useState("seller");
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        {!isLoggedIn
+        ?
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        :
+        (role === 'seller' 
+         ?
+         <Stack.Screen name="(seller)" options={{ headerShown: false }} />
+         :
+         <Stack.Screen name="(buyer)" options={{ headerShown: false }} />
+        )
+        }
+        
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
