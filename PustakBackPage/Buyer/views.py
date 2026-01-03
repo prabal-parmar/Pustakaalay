32dc9ebf5f08ad0e01bca45f723e118c4bc62688
Prabal Parmar <prabal9713@gmail.com>
2026-01-04 03:56:35 +0530

    Add view for buyer profile for profile tab for buyer section

--- PustakBackPage/Buyer/views.py
+++ PustakBackPage/Buyer/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+from Users.models import BuyerModel, CustomUser
+from .models import BuyerProfile
 
 # Buyer Profile
 @api_view(['GET'])
 def get_profile(request):
-    return Response({"data": "Buyer's Profile"})
+    username = request.query_params.get("username")
+    user = CustomUser.objects.filter(username=username).first()
+    buyer = BuyerModel.objects.filter(user=user).first()
+    buyer_profile = BuyerProfile.objects.filter(buyer=buyer).first()
+
+    if buyer_profile is not None:
+        data = {
+            "name" : f"{buyer_profile.buyer.user.first_name} {buyer_profile.buyer.user.last_name}",
+            "username" : buyer_profile.buyer.user.username,
+            "location" : buyer_profile.buyer.city,
+            "following" : buyer_profile.following,
+            "followers" : buyer_profile.followers,
+            "description" : buyer_profile.description,
+        }
+        return Response(
+            {"data": data,
+            "message": "Buyer Profile data sent"}, 
+            status=status.HTTP_200_OK)
+    else:
+        return Response(
+            {"data": None,
+            "message": "Unable to fetch buyer profile data"}, 
+            status=status.HTTP_204_NO_CONTENT)
 
 # Send Buy book request
 @api_view(['GET'])
