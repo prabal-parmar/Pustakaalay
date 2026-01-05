cb8219910fdc50aa3aef30998352015b1b11643a
Prabal Parmar <prabal9713@gmail.com>
2026-01-06 01:20:40 +0530

    Add view for add_book by seller

--- PustakBackPage/Seller/urls.py
+++ PustakBackPage/Seller/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from Seller import views
 
 urlpatterns = [
-    path('profile/', views.get_profile, name="Seller Profile Page")
+    path('profile/', views.get_profile, name="Seller Profile Page"),
+    path('add-book/', views.add_book, name="Add New Book")
 ]
--- PustakBackPage/Seller/views.py
+++ PustakBackPage/Seller/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from Users.models import SellerModel, CustomUser
-from .models import SellerProfile
+from .models import SellerProfile, BookDataModel
 from rest_framework import status
 
 # Seller profile
@@ -32,6 +32,34 @@ def get_profile(request):
             {"data": None,
             "message": "Unable to fetch seller profile data"}, 
             status=status.HTTP_204_NO_CONTENT)
+
+# Add book to sell with data
+@api_view(['POST'])
+def add_book(request):
+    name = request.data.get("name")
+    author = request.data.get("author")
+
+    find_book = BookDataModel.objects.filter(name=name, author=author).first()
+
+    if find_book is not None:
+        return Response({"message": "Book already added.", "completed": False}, status=status.HTTP_200_OK)
+    
+    description = request.data.get("description")
+    price = request.data.get("price")
+    quantity = request.data.get("quantity")
+    educational_content = request.data.get("educational_content")
+    category = request.data.get("category")
+    condition = request.data.get("condition")
+    BookDataModel.objects.create(name=name, 
+                                 author=author,
+                                 description=description,
+                                 price=price,
+                                 quantity=quantity,
+                                 educational_content=educational_content,
+                                 category=category,
+                                 condition=condition)
+    
+    return Response({"message": f"{name} added successfully.", "completed": True}, status=status.HTTP_201_CREATED)
 
 def update_profile(request):
     return Response({"data": "Update Profile"})
