08dce9ca44bfc4521ac100fef8bda95b42d7c940
Prabal Parmar <prabal9713@gmail.com>
2026-01-08 22:49:06 +0530

    Add condition and refine other api calls for bookForm

--- PustakFrontPage/api/sellerApis/addBookForm.tsx
+++ PustakFrontPage/api/sellerApis/addBookForm.tsx
@@ -8,9 +8,9 @@ export const addBookData = async (data: any) => {
             description: data.description,
             price: data.price,
             quantity: data.quantity,
-            category: data.genre,
+            category: data.genre.toLowerCase(),
             educational_content: data.isEducational,
-            condition: "new"
+            condition: data.condition.toLowerCase()
         }
         const response = await api.post('/seller/add-book/', formData);
         return [response.data.message, response.data.completed]
