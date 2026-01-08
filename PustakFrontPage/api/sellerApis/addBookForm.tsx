3d8ae599d8edfe88175e2c9274f7299187d0c712
Prabal Parmar <prabal9713@gmail.com>
2026-01-09 00:22:27 +0530

    Update seller Api for book form for category of the book

--- PustakFrontPage/api/sellerApis/addBookForm.tsx
+++ PustakFrontPage/api/sellerApis/addBookForm.tsx
@@ -1,17 +1,21 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { api } from "../authApis/api";
 
 export const addBookData = async (data: any) => {
     try {
+        const username = await AsyncStorage.getItem("username");
         const formData = {
-            title: data.title.toLowerCase(),
+            name: data.title.toLowerCase(),
             author: data.author.toLowerCase(),
             description: data.description,
             price: data.price,
             quantity: data.quantity,
-            category: data.genre.toLowerCase(),
+            category: data.type.toLowerCase(),
             educational_content: data.isEducational,
-            condition: data.condition.toLowerCase()
+            condition: data.condition.toLowerCase(),
+            username: username?.toLowerCase()
         }
+        console.log(formData)
         const response = await api.post('/seller/add-book/', formData);
         return [response.data.message, response.data.completed]
 
