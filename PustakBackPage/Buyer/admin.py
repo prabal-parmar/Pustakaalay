d0f266722dc71b3bb146f8f90d1c434c959f4255
Prabal Parmar <prabal9713@gmail.com>
2026-01-15 21:24:47 +0530

    Add BookForSellBuyer model for buyer to add book for selling

--- PustakBackPage/Buyer/admin.py
+++ PustakBackPage/Buyer/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import BuyerProfile
+from .models import BuyerProfile, BookForSellBuyer
 # Register your models here.
 
 @admin.register(BuyerProfile)
 class SellerProfileAdmin(admin.ModelAdmin):
     list_display = [field.name for field in BuyerProfile._meta.fields]
+
+@admin.register(BookForSellBuyer)
+class SellerProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in BookForSellBuyer._meta.fields]
--- PustakBackPage/Buyer/models.py
+++ PustakBackPage/Buyer/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from Users.models import BuyerModel
+import uuid
 # Create your models here.
 
 class BuyerProfile(models.Model):
@@ -10,3 +11,35 @@ class BuyerProfile(models.Model):
     
     def __str__(self):
         return f"{self.buyer.user.username}"
+
+
+class BookForSellBuyer(models.Model):
+    CATEGORY_TYPE = (
+        ("novel", "Novel"),
+        ("historical", "Historical"),
+        ("biography", "Biography"),
+        ("scientific", "Scientific"),
+        ("miscellaneous", "Miscellaneous"),
+        ("other", "Other")
+    )
+
+    NOVEL_GENRE_TYPE = (
+        ("fiction", "Fiction"),
+        ("fantasy", "Fantasy"),
+        ("mystery", "Mystery"),
+        ("romance", "Romance"),
+        ("sci-fi", "Sci-fi"),
+        ("thriller", "Thriller")
+    )
+
+    book_id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    buyer = models.ForeignKey(BuyerModel, on_delete=models.CASCADE, related_name="buyer_books_sell")
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(default="None")
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_TYPE)
+    genre = models.CharField(max_length=10, choices=NOVEL_GENRE_TYPE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.buyer.user.username}"
