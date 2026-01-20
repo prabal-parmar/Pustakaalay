c1a332b22d3a170ae6ecba53c23c7ced276e08fd
Prabal Parmar <prabal9713@gmail.com>
2026-01-20 22:52:22 +0530

    Add url for selling new book by buyer

--- PustakBackPage/Buyer/urls.py
+++ PustakBackPage/Buyer/urls.py
@@ -3,4 +3,5 @@ from Buyer import views
 
 urlpatterns = [
     path('profile/', views.get_profile, name="Buyers urls"),
+    path('sell-book/', views.sell_book_to_others, name="Add book to sell")
 ]
