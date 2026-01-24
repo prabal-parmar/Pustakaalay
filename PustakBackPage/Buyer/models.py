fa48570119f98ef98930af6a6a7e80c02fba5bcc
Prabal Parmar <prabal9713@gmail.com>
2026-01-24 19:54:03 +0530

    Add models for new ebook to be added by buyer

--- PustakBackPage/Buyer/models.py
+++ PustakBackPage/Buyer/models.py
@@ -13,9 +13,7 @@ class BuyerProfile(models.Model):
     def __str__(self):
         return f"{self.buyer.user.username}"
 
-
-class BookForSellBuyer(models.Model):
-    CATEGORY_TYPE = (
+CATEGORY_TYPE = (
         ("novel", "Novel"),
         ("historical", "Historical"),
         ("biography", "Biography"),
@@ -24,20 +22,37 @@ class BookForSellBuyer(models.Model):
         ("other", "Other")
     )
 
-    NOVEL_GENRE_TYPE = (
-        ("fiction", "Fiction"),
-        ("fantasy", "Fantasy"),
-        ("mystery", "Mystery"),
-        ("romance", "Romance"),
-        ("sci-fi", "Sci-fi"),
-        ("thriller", "Thriller")
-    )
+NOVEL_GENRE_TYPE = (
+    ("fiction", "Fiction"),
+    ("fantasy", "Fantasy"),
+    ("mystery", "Mystery"),
+    ("romance", "Romance"),
+    ("sci-fi", "Sci-fi"),
+    ("thriller", "Thriller")
+)
 
+
+class EbookModel(models.Model):
+    ebook_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    buyer = models.ForeignKey(BuyerModel, on_delete=models.CASCADE, related_name="buyer_ebook")
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(default="No Description")
+    category = models.CharField(max_length=20, choices=CATEGORY_TYPE)
+    genre = models.CharField(max_length=10, choices=NOVEL_GENRE_TYPE, null=True, blank=True)
+    date = models.DateField(default=timezone.now)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} - {self.buyer.user.username}"
+    
+
+class BookForSellBuyer(models.Model):
     book_id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
     buyer = models.ForeignKey(BuyerModel, on_delete=models.CASCADE, related_name="buyer_books_sell")
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    description = models.TextField(default="None")
+    description = models.TextField(default="No Description")
     price = models.DecimalField(max_digits=6,decimal_places=2)
     category = models.CharField(max_length=20, choices=CATEGORY_TYPE)
     genre = models.CharField(max_length=10, choices=NOVEL_GENRE_TYPE, null=True, blank=True)
