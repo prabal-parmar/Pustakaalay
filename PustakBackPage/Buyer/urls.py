8c8828890b52f8c379a246cf89352ceb2927c21d
Prabal Parmar <prabal9713@gmail.com>
2026-03-28 19:41:07 +0530

    Add view and url for adding new book to exchange

--- PustakBackPage/Buyer/urls.py
+++ PustakBackPage/Buyer/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('profile/', views.get_profile, name="Buyers urls"),
     path('sell-book/', views.add_new_book_to_sell, name="Add book to sell"),
     path('e-book/', views.add_new_ebook, name="Add new e-book"),
-    path('my-sell-books/', views.buyer_books_to_sell, name="Buyer Book to sell"),
+    path('exchange-book/', views.add_exchange_book, name="Add New Book to Exchange"),
+    path('my-sell-books/', views.fetch_buyer_book, name="Buyer Book to sell"),
     path('my-ebooks/', views.fetch_buyer_ebook, name="Buyer All E-book"),
     path('<str:username>/ebook/<str:ebook_id>/', views.viewed_ebook, name="Open Ebook with id")
 ]
--- PustakBackPage/Buyer/views.py
+++ PustakBackPage/Buyer/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from Users.models import BuyerModel, CustomUser
-from .models import BuyerProfile, BookForSellBuyer, EbookModel, EbookHistory
+from .models import BuyerProfile, BookForSellBuyer, EbookModel, EbookHistory, ExchangeBookModel
 from django.utils import timezone
 
 # Buyer Profile
@@ -103,6 +103,49 @@ def add_new_ebook(request):
     
     return Response({"message": f"{name} uploaded successfully.", "completed": True}, status=status.HTTP_201_CREATED)
 
+# Add new book to exchange
+@api_view(['POST'])
+def add_exchange_book(request):
+    username = request.data.get("username")
+
+    user = CustomUser.objects.filter(username=username).first()
+    buyer = BuyerModel.objects.filter(user=user).first()
+    if not buyer:
+        return Response({"message": "Unable to find buyer.", 
+                         "data": None, 
+                         "completed": False}, status=status.HTTP_404_NOT_FOUND)
+    
+    name = request.data.get("myBookName")
+    author = request.data.get("myAuthor")
+    already_added = ExchangeBookModel.objects.filter(buyer=buyer, name=name, author=author).first()
+    if already_added:
+        return Response({"message": "Book already added.", 
+                         "data": already_added.book_id, 
+                         "completed": True}, status=status.HTTP_200_OK)
+
+    category = request.data.get("myCategory")
+    genre = request.data.get("myGenre")
+    condition = request.data.get("myCondition")
+    desired_category = request.data.get("desiredCategory")
+    desired_genre = request.data.get("desiredGenre")
+    wanted_condition = request.data.get("desiredCondition")
+    description = request.data.get("exchangeNotes")
+
+    ExchangeBookModel.objects.create(buyer=buyer,
+                                     name=name,
+                                     author=author,
+                                     category=category,
+                                     genre=genre,
+                                     condition=condition,
+                                     desired_category=desired_category,
+                                     desired_genre=desired_genre,
+                                     wanted_condition=wanted_condition,
+                                     description=description)
+    
+    return Response({"message": "New Book to Exchange added.", 
+                     "data": None, 
+                     "completed": True}, status=status.HTTP_201_CREATED)
+
 # Fetch all E-book of buyer
 @api_view(['GET'])
 def fetch_buyer_ebook(request):
@@ -132,9 +175,9 @@ def fetch_buyer_ebook(request):
         ebook_data.append(data)
     return Response({"message": "Buyer E-books sent", "data": ebook_data, "completed": True}, status=status.HTTP_200_OK)
 
-# Buyer books to sell
+# Fetch all books of buyer
 @api_view(['GET'])
-def buyer_books_to_sell(request):
+def fetch_buyer_book(request):
     username = request.query_params.get("username")
     user = CustomUser.objects.filter(username=username).first()
     buyer = BuyerModel.objects.filter(user=user).first()
@@ -184,21 +227,21 @@ def all_exchange_book_requests(request):
 def viewed_ebook(request, ebook_id, username):
     user = CustomUser.objects.filter(username=username).first()
     if not user:
-        return Response({"message": "Can not find User", "data": None})
+        return Response({"message": "Can not find User", "data": None}, status=status.HTTP_404_NOT_FOUND)
     
     buyer = BuyerModel.objects.filter(user=user).first()
     if not buyer:
-        return Response({"message": "Can not find buyer", "data": None})
+        return Response({"message": "Can not find buyer", "data": None}, status=status.HTTP_404_NOT_FOUND)
     
     ebook = EbookModel.objects.filter(ebook_id=ebook_id).first()
     if not ebook:
-        return Response({"message": f"No E-book found with id: {ebook_id}", "data": None})
+        return Response({"message": f"No E-book found with id: {ebook_id}", "data": None}, status=status.HTTP_404_NOT_FOUND)
     
     check_view = EbookHistory.objects.filter(ebook_id=ebook, buyer_seen=buyer)
     if check_view:
-        return Response({"message": "Already Opened", "data": {"id": None}})
+        return Response({"message": "Already Opened", "data": {"id": None}}, status=status.HTTP_200_OK)
     else:
         ebook.views += 1
         ebook.save()
         EbookHistory.objects.create(ebook_id=ebook, buyer_seen=buyer).save()
-        return Response({"message": "Views Updated", "data": { "id": ebook_id }})
+        return Response({"message": "Views Updated", "data": { "id": ebook_id }}, status=status.HTTP_200_OK)
