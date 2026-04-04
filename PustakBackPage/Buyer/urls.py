d6d299705542891fc4d72581b49a077b3880d574
Prabal Parmar <prabal9713@gmail.com>
2026-04-04 19:32:35 +0530

    Add path and other changes in backend

--- PustakBackPage/Buyer/urls.py
+++ PustakBackPage/Buyer/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('exchange-books/', views.fetch_buyer_exchange_book, name="Send all exchange books of buyer"),
     path('<str:username>/ebook/<str:ebook_id>/', views.viewed_ebook, name="Open Ebook with id"),
     path('hot-picks/', views.get_hot_ebook_picks, name="Hot Ebook data for Buyer"),
-    path('trade-history/', views.get_trade_history, name="Fetch Trade History of books.")
+    path('trade-history/', views.get_trade_history, name="Fetch Trade History of books."),
+    path('rec-exchange/', views.get_local_exchange, name="Local Exchange for Buyer")
 ]
