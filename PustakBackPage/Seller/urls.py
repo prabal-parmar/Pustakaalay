384ad661bf79ecd667c9a877af11e032280382ed
Prabal Parmar <prabal9713@gmail.com>
2026-04-08 02:08:19 +0530

    Add view for fetching recent inventory of books for seller with url of it

--- PustakBackPage/Seller/urls.py
+++ PustakBackPage/Seller/urls.py
@@ -4,5 +4,6 @@ from Seller import views
 urlpatterns = [
     path('profile/', views.get_profile, name="Seller Profile Page"),
     path('add-book/', views.add_book_to_sell, name="Add New Book"),
-    path('my-all-books/', views.seller_books_data, name="Seller all books data")
+    path('my-all-books/', views.seller_books_data, name="Seller all books data"),
+    path('my-inventory/', views.fetch_recent_inventory, name="Seller Books Inventory")
 ]
--- PustakBackPage/Seller/views.py
+++ PustakBackPage/Seller/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from Users.models import SellerModel, CustomUser
-from .models import SellerProfile, BookDataModel
+from .models import SellerProfile, BookDataModel, BookHistoryModel
 from rest_framework import status
 
 # Seller profile
@@ -11,6 +11,10 @@ def get_profile(request):
     username = request.query_params.get("username")
     user = CustomUser.objects.filter(username=username).first()
     seller = SellerModel.objects.filter(user=user).first()
+    if seller is None:
+        return Response({"message": "User not found", "data": None, "completed": True}, 
+                        status=status.HTTP_404_NOT_FOUND)
+    
     seller_profile = SellerProfile.objects.filter(seller=seller).first()
 
     if seller_profile is not None:
@@ -41,7 +45,10 @@ def add_book_to_sell(request):
     username = request.data.get("username")
     user = CustomUser.objects.filter(username=username).first()
     seller = SellerModel.objects.filter(user=user).first()
-
+    if seller is None:
+        return Response({"message": "User not found", "data": None, "completed": True}, 
+                        status=status.HTTP_404_NOT_FOUND)
+    
     find_book = BookDataModel.objects.filter(name=name, author=author, seller=seller).first()
 
     if find_book is not None:
@@ -67,13 +74,17 @@ def add_book_to_sell(request):
     
     return Response({"message": f"{name} added successfully.", "completed": True}, status=status.HTTP_201_CREATED)
 
+# Fetch Seller books data
 @api_view(['GET'])
 def seller_books_data(request):
-
     username = request.query_params.get("username")
 
     user = CustomUser.objects.filter(username=username).first()
     seller = SellerModel.objects.filter(user=user).first()
+    if seller is None:
+        return Response({"message": "User not found", "data": None, "completed": True}, 
+                        status=status.HTTP_404_NOT_FOUND)
+    
     all_books = BookDataModel.objects.filter(seller=seller).values()
 
     books = []
@@ -95,6 +106,7 @@ def seller_books_data(request):
                      "message": "All books by seller sent", 
                      "completed": True}, status=status.HTTP_200_OK)
 
+# Update seller profile
 def update_profile(request):
     return Response({"data": "Update Profile"})
 
@@ -117,3 +129,31 @@ def sell_book_seller(request):
 @api_view(['GET'])
 def seller_all_books(request):
     return Response({"data": "Seller All Books"})
+
+# Recent Inventory for seller home page
+@api_view(['GET'])
+def fetch_recent_inventory(request):
+    username = request.query_params.get('username')
+    user = CustomUser.objects.filter(username=username).first()
+    seller = SellerModel.objects.filter(user=user).first()
+    if seller is None:
+        return Response({"message": "User not found", 
+                         "data": None, 
+                         "completed": True}, status=status.HTTP_404_NOT_FOUND)
+    
+    recent_books = list(BookDataModel.objects.filter(seller=seller).order_by('-created_at')[:2])
+    
+    book_data = []
+    for book in recent_books:
+        temp = {
+            "id": book.book_id,
+            "title": book.name,
+            "price": float(book.price),
+            "views": book.views
+        }
+
+        book_data.append(temp)
+    
+    return Response({"message": "My Inventory data sent.", 
+                     "data": book_data, 
+                     "completed": True}, status=status.HTTP_200_OK)
