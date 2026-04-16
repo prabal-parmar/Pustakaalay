68afeee4e3f517b559e7596987f531e4309deda9
Prabal Parmar <prabal9713@gmail.com>
2026-04-16 19:41:49 +0530

    Add explore page apis for buyer of fetching books, ebooks and exchange books

---
+++ PustakFrontPage/api/buyerApis/exploreApi.tsx
@@ -0,0 +1,22 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { api } from "../authApis/api";
+
+const getUsername = async () => {
+    const username = await AsyncStorage.getItem("username");
+    if (username){
+        return username.toLowerCase();
+    }
+    return ""
+}
+
+export const fetchBooksEbooksForBuyer = async () => {
+    try {
+        const username = await getUsername();
+        const response = await api.get('/buyer/explore/', {params: {username: username}})
+
+        return [response.data.message, response.data.data, response.data.completed]
+    } catch (error) {
+        console.log(error)
+        return ["Something went wrong!", null, false]
+    }
+}
