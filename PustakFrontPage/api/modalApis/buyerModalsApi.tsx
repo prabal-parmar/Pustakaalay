5d749bbed8e846e08df20bd660e5669d9a768a57
Prabal Parmar <prabal9713@gmail.com>
2026-04-20 20:44:07 +0530

    Add api call for fetching book, ebook or exchange book data by id and type

--- PustakFrontPage/api/modalApis/buyerModalsApi.tsx
+++ PustakFrontPage/api/modalApis/buyerModalsApi.tsx
@@ -38,3 +38,31 @@ export const fetchBookOrEbookDataById = async (id: string, type: string) => {
         return ["Something went wrong!", null, false]
     }
 }
+
+export const fetchMyBookEbookExchangeBookById = async (id: string, type: string) => {
+    try {
+        const username = await getUsername();
+        if(!username){
+            throw new Error("Unable to fetch username!")
+        }
+        var response;
+        if(type == "selling"){
+            response = await api.get(`/buyer/mybooks/my/buy/${id}/`, {params: {username: username}})
+            return [response.data.message, response.data.data, response.data.completed]
+        }
+        else if(type == "library"){
+            response = await api.get(`/buyer/mybooks/my/ebook/${id}/`, {params: {username: username}})
+            return [response.data.message, response.data.data, response.data.completed]
+        }
+        else if(type == "exchange"){
+            response = await api.get(`/buyer/mybooks/my/exchange/${id}/`, {params: {username: username}})
+            return [response.data.message, response.data.data, response.data.completed]
+        }
+        else{
+            throw new Error("Wrong type of Book selected!")
+        }
+    } catch (error: any) {
+        console.log(error.message)
+        return ["Something went wrong!", null, false]
+    }
+}
